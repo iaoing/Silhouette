56e6e4fd25d8b69f067d4d45b90a101bc323d03f
Bing Jiao <jiao@cs.fsu.edu>
2024-12-25 03:30:46 -0500

    fixed an issue

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# After installing memcached, it will be auto started by the systemctl. Disabling it to avoid any issues.
+sudo systemctl stop memcached
+sudo systemctl disable memcached
+
 cd codebase/scripts/fs_conf/sshkey
 chmod 600 fast25_ae_vm
 chmod 644 fast25_ae_vm.pub
